72ffdb43e745f1c8db499aca21b0ae99effdfcd7
PanatdaButwiangphan <butwiangphan38@gmail.com>
2025-05-31 01:22:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ map.on('click', e => {
 });
 
 
-fetch('miniProject_/data/clip_with_traffic.geojson')
+fetch('/data/clip_with_traffic.geojson')
   .then(r => r.json())
   .then(data => {
     allFeatures = data.features;
